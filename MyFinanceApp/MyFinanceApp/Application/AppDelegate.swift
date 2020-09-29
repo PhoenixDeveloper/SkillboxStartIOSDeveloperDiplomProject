869832c7ac83860a19c7ea8418726e84e0f69cbb
Mikhail Belenko <mefisto40000x@gmail.com>
2020-09-29 14:12:58 +0300

    Create project and import libraries

---
+++ MyFinanceApp/MyFinanceApp/Application/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  MyFinanceApp
+//
+//  Created by Михаил Беленко on 29.09.2020.
+//  Copyright © 2020 MOB. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let rootController = RootTabBarController()
+
+        window!.rootViewController = rootController
+        window!.makeKeyAndVisible()
+
+        return true
+    }
+}
+
---
+++ MyFinanceApp/MyFinanceApp/Core/RootTabBar/RootTabBarController.swift
@@ -0,0 +1,30 @@
+//
+//  RootTabBarController.swift
+//  MyFinanceApp
+//
+//  Created by Михаил Беленко on 29.09.2020.
+//  Copyright © 2020 MOB. All rights reserved.
+//
+
+import UIKit
+
+class RootTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
